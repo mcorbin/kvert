b9a9114125b5be04c725e58afaff8b0109871c24
mcorbin <corbin.math@gmail.com>
2021-12-26 15:41:55 +0100

    repositories spec

--- src/ymlgen/spec.clj
+++ src/ymlgen/spec.clj
@@ -1,6 +1,7 @@
 (ns ymlgen.spec
   (:require [clojure.java.io :as io]
-            [clojure.spec.alpha :as s]))
+            [clojure.spec.alpha :as s]
+            [clojure.string :as string]))
 
 (defn file?
   "Verifies if the provided path is a file"
@@ -9,11 +10,31 @@
     (and (.exists file)
          (.isFile file))))
 
+(s/def ::ne-string (s/and string? (complement string/blank?)))
 (s/def ::variables (s/map-of keyword? any?))
 (s/def ::profile keyword?)
-(s/def ::path string?)
+(s/def ::path ::ne-string)
+(s/def ::url ::ne-string)
 
-(s/def ::config (s/keys :opt-un [::variables ::profile]))
+(s/def ::cacert file?)
+(s/def ::key file?)
+(s/def ::cert file?)
+(s/def ::name ::ne-string)
+
+(s/def ::repository (s/keys :req-un [::name ::url]
+                            :opt-un [::cacert
+                                     ::key
+                                     ::cert]))
+
+(s/def ::repositories (s/and (s/coll-of ::repository)
+                             (fn [repositories]
+                               (= (count repositories)
+                                  (->> repositories
+                                       (map :name)
+                                       set
+                                       count)))))
+
+(s/def ::config (s/keys :opt-un [::variables ::profile ::repositories]))
 
 (s/def ::include (s/keys :req-un [::path]
                          :opt-un [::variables ::profile]))
---
+++ test/ymlgen/spec_test.clj
@@ -0,0 +1,13 @@
+(ns ymlgen.spec-test
+  (:require [clojure.spec.alpha :as s]
+            [clojure.test :refer :all]
+            [ymlgen.spec :as spec]))
+
+(deftest repositories-test
+  (is (s/valid? ::spec/repositories []))
+  (is (s/valid? ::spec/repositories [{:name "foo" :url "http://mcorbin.fr"}]))
+  (is (not (s/valid? ::spec/repositories [{:name "foo" :url "http://mcorbin.fr"}
+                                          {:name "foo" :url "http://mcorbin.fr"}
+                                          ])))
+  (is (s/valid? ::spec/repositories [{:name "foo" :url "http://mcorbin.fr"}
+                                     {:name "bar" :url "http://mcorbin.fr"}])))
